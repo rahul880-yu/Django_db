afe6d02381dea13d064594843044fa6b18fdb64a
rahul <misterrahulchauhan@gmail.com>
2024-08-08 11:23:19 +0530

    added migrated file

--- store/models.py
+++ store/models.py
@@ -13,6 +13,7 @@ class Collection(models.Model):
 class Product(models.Model):
     collection=models.ForeignKey(Collection, on_delete=models.PROTECT)
     promotions=models.ManyToManyField(Promotion)
+    slug=models.SlugField(default='-')
     title=models.CharField(max_length=255)
     description=models.TextField()
     price=models.DecimalField(max_digits=6, decimal_places=2)
--- storefront/settings.py
+++ storefront/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "playground",
     "store",
     "tags",
+    'likes'
 ]
 
 MIDDLEWARE = [
@@ -78,8 +79,11 @@ WSGI_APPLICATION = "storefront.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": 'storefront',
+        "HOST": 'localhost',
+        "USER": 'root',
+        "PASSWORD": 'pass123',
     }
 }
 
